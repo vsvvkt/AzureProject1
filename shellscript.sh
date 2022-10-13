c92cfa07bff84b166724405ad2b6a3dff3521269
Cloud User <azureuser@azureproject1.ps0pktgvhtuunm3in2xe5xajsc.rx.internal.cloudapp.net>
2022-10-13 13:16:46 +0000

    shellscriptfile

---
+++ shellscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "docker version:-"
+docker --version
+echo "ansible version:-"
+ansible --version
+echo "java version:-"
+java --version
+echo "python3 version:-"
+python3 --version
+echo "pip3 version:-"
+pip3 --version
